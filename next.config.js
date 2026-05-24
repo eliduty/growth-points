27680e07dd0caf5bf76445dc9943e545322668d6
张骄龙 <zhangjiaolong@yineng.com.cn>
2026-05-24 23:58:38 +0800

    build(next.config): add experimental server components external packages
    配置Next.js实验性属性，添加Prisma和libsql相关包作为外部组件依赖

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "@prisma/adapter-libsql", "@libsql/client", "prisma"],
+  },
+};
 
 module.exports = nextConfig;
